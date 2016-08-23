fbf4e1367be555fb0d16eb47098aeef4e88caf78
Aedan Smith <aedan.smith@gmail.com>
2016-08-23 15:55:51 -0700

    Added DisplayManager.

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,6 @@
+import com.aedans.engine.DisplayManager;
+import org.lwjgl.LWJGLException;
+
 /**
  * Created by Aedan Smith on 8/23/2016.
  *
@@ -12,7 +15,12 @@ public class Main {
      * @param args: List of arguments for the game.
      */
     public static void main(String[] args){
-
+        try {
+            DisplayManager.createDisplay(Integer.parseInt(args[0]), Integer.parseInt(args[1]), false, "F2P Platformer");
+        } catch (Exception e){
+            System.out.print("Could not initialize game: ");
+            e.printStackTrace(System.out);
+        }
     }
 
 }
---
+++ src/com/aedans/engine/DisplayManager.java
@@ -0,0 +1,73 @@
+package com.aedans.engine;
+
+import org.lwjgl.LWJGLException;
+import org.lwjgl.opengl.*;
+
+/**
+ * Created by Aedan Smith on 5/3/2016.
+ * <p>
+ * The Display manager for the Game.
+ */
+
+public class DisplayManager {
+
+    /**
+     * The x and y resolution of the Display.
+     */
+    public static int xRes, yRes;
+
+    /**
+     * The FPS to cap the Display at.
+     */
+    private static int fpscap = Display.getDesktopDisplayMode().getFrequency();
+
+    /**
+     * Creates a display.
+     *
+     * @param xRes:       The x resolution of the Display.
+     * @param yRes:       The y resolution of the Display.
+     * @param fullscreen: True if the Display should initialize in fullscreen.
+     * @param title:      The title of the display (as seen at the top)
+     *                    TODO: Remove Display Title.
+     * @throws LWJGLException If LWJGL could not initialize the Display.
+     */
+    public static void createDisplay(int xRes, int yRes, boolean fullscreen, String title) throws LWJGLException {
+        DisplayManager.xRes = xRes;
+        DisplayManager.yRes = yRes;
+
+        ContextAttribs attributes = new ContextAttribs(3, 2)
+                .withForwardCompatible(true)
+                .withProfileCore(true);
+
+        Display.setDisplayMode(new DisplayMode(xRes, yRes));
+        Display.setResizable(false);
+        Display.setFullscreen(fullscreen);
+        Display.create(new PixelFormat(), attributes);
+        Display.setTitle(title);
+        GL11.glViewport(0, 0, xRes, yRes);
+        System.out.println(GL11.glGetString(GL11.GL_VERSION));
+    }
+
+    /**
+     * @return if the user has requested to close the Display.
+     */
+    public static boolean isCloseRequested() {
+        return Display.isCloseRequested();
+    }
+
+    /**
+     * Updates the Display.
+     */
+    public static void updateDisplay() {
+        Display.sync(fpscap);
+        Display.update();
+    }
+
+    /**
+     * Closes the Display.
+     */
+    public static void closeDisplay() {
+        Display.destroy();
+    }
+
+}
