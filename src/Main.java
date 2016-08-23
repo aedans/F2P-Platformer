5bee412cc4a608ad6d6e04b19727235f8eb36716
Aedan Smith <aedan.smith@gmail.com>
2016-08-23 15:41:03 -0700

    Initial commit.

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+/**
+ * Created by Aedan Smith on 8/23/2016.
+ *
+ * The Main class for the Game.
+ */
+
+public class Main {
+
+    /**
+     * The Main function for the Game.
+     *
+     * @param args: List of arguments for the game.
+     */
+    public static void main(String[] args){
+
+    }
+
+}
